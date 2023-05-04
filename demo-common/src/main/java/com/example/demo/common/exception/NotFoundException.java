213ee5ed5b423e13cbc7dd718c90e614d4861b75
Bomou He <paakmau@outlook.com>
2023-05-04 16:51:54 +0800

    feat: add simple exception handling

---
+++ demo-common/src/main/java/com/example/demo/common/exception/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.demo.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Arrays;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(Class<?> resourceClass, String[] conditions) {
+        super(
+                String.format(
+                        "The request for %s failed, find by %s",
+                        resourceClass.getSimpleName(), Arrays.toString(conditions)));
+    }
+}
