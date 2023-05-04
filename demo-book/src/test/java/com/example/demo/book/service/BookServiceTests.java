f4fc84c092dc741ad1bee9690113128d0f92dca8
Bomou He <paakmau@outlook.com>
2023-05-04 17:30:15 +0800

    test: add unit test for BookService

---
+++ demo-book/src/test/java/com/example/demo/book/service/BookServiceTests.java
@@ -0,0 +1,75 @@
+package com.example.demo.book.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.book.dto.BookDto;
+import com.example.demo.book.entity.Book;
+import com.example.demo.book.mapper.BookMapper;
+import com.example.demo.book.repo.BookRepo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class BookServiceTests {
+    private static List<Book> books;
+    private static List<BookDto> bookDtos;
+
+    @MockBean private BookRepo repo;
+
+    @Autowired private BookService service;
+
+    @BeforeAll
+    static void initAll() {
+        books =
+                Arrays.asList(
+                        new Book(1L, "Book 1", "Author 1", "Description 1"),
+                        new Book(2L, "Book 2", "Author 2", "Description 2"),
+                        new Book(3L, "Book 3", "Author 3", "Description 3"));
+
+        bookDtos = books.stream().map(BookMapper.INSTANCE::toDto).collect(Collectors.toList());
+    }
+
+    @BeforeEach
+    void init() {}
+
+    @Test
+    void testGet() {
+        for (Book book : books) {
+            Mockito.when(repo.findById(book.getId())).thenReturn(Optional.of(book));
+        }
+        for (BookDto dto : bookDtos) {
+            assertEquals(dto, service.get(dto.id()));
+        }
+    }
+
+    @Test
+    void testGetByTitle() {
+        for (Book book : books) {
+            Mockito.when(repo.findByTitle(book.getTitle())).thenReturn(Arrays.asList(book));
+        }
+        for (BookDto dto : bookDtos) {
+            List<BookDto> vos = service.getByTitle(dto.title());
+            assertEquals(1, vos.size());
+            assertEquals(dto, vos.get(0));
+        }
+    }
+
+    @AfterEach
+    void tearDown() {}
+
+    @AfterAll
+    static void tearDownAll() {}
+}
